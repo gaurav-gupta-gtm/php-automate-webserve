7fbcdd54e67c7f9cd19c635e4820608b7714b694
Gaurav Gupta <gauravguptagtm@gmail.com>
2020-06-03 00:49:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
+dy>
+
+<h1>My first PHP page</h1>
+
 <?php
 
-echo "This is Gaurav Gupta"
-echo "Successfully connected"
+echo -e  "This is Gaurav Gupta/n ";
+echo "Successfully connected to web-server";
 
 ?>
+</body>
+</html>
